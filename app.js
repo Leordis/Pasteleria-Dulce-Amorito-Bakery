b2c0b6cc716e70fe8f557a5e5e9e7ebd25c13de8
Leordis Amaro <perroperroplay@gmail.com>
2026-06-21 15:47:35 -0400

    Usar foto original de Quinceañera con fondo temático rosa

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ const PRODUCTS = {
     },
     {
       id: 't3', name: 'Quinceañera Cake', desc: 'Dream design for your most special day', emoji: '👑',
-      img: "torta%20de%20quinceañera/quinceanera-red-cake.png", price: 95, note: 'Price from'
+      img: "torta%20de%20quinceañera/quinceanera-red-cake.jpg", price: 95, note: 'Price from'
     },
     {
       id: 't4', name: 'Birthday Cake', desc: 'Customized with your favorite colors, theme, and flavors', emoji: '🎉',
